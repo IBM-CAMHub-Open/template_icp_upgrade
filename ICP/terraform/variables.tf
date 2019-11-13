b99224a7b52df0e84460ec28b5988092f671ea06
CAMHub PublishBot <octravis@us.ibm.com>
2019-11-13 22:15:44 +0000

    Automated CAMHub::publish on 2019-11-13 22:15:44 UTC

---
+++ ICP/terraform/variables.tf
@@ -0,0 +1,51 @@
+variable "vm_os_password" {
+  type = "string"
+}
+
+variable "vm_os_user" {
+  type = "string"
+}
+
+variable "icp_private_ssh_key" {
+  type = "string"
+  default = ""
+}
+
+variable "boot_vm_ipv4_address" {
+  type = "string"
+}
+
+variable "new_icp_version" {
+  type = "string"
+  default = "3.2.1"
+}
+
+variable "new_icp_binary_url" {
+  type = "string"
+}
+
+variable "download_user" {
+  type = "string"
+}
+
+variable "download_user_password" {
+  type = "string"
+}
+
+variable "cluster_location" {
+  type = "string"
+}
+
+variable "icp_cluster_name" {
+  type = "string"
+  default = "mycluster"
+}
+
+variable "master_node_ip" {
+  type = "string"
+}
+
+variable "kube_apiserver_secure_port" {
+  type = "string"
+  default = "8001"
+}
